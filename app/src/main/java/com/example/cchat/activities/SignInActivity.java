72d1620c815a647f14df2c39883da0058dc2a307
lxy <2803977318@qq.com>
2022-05-13 18:53:34 +0800

    5.13login按钮

--- app/src/main/java/com/example/cchat/activities/SignInActivity.java
+++ app/src/main/java/com/example/cchat/activities/SignInActivity.java
@@ -1,5 +1,9 @@
 package com.example.cchat.activities;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
@@ -9,9 +13,14 @@ import android.os.Bundle;
 import com.example.cchat.R;
 import com.example.cchat.databinding.ActivitySignInBinding;
 
+import static android.app.ProgressDialog.show;
+
 public class SignInActivity extends AppCompatActivity {
 
     private ActivitySignInBinding binding;
+    private EditText inputEmail;
+    private EditText inputPassword;
+    private Button signInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,29 @@ public class SignInActivity extends AppCompatActivity {
         binding = ActivitySignInBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setListeners();
+        inputEmail = findViewById(R.id.inputEmail);
+        inputPassword = findViewById(R.id.inputPassword);
+        signInButton = findViewById(R.id.buttonSignin);
+        signInButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String email = inputEmail.getText().toString().trim();
+                String pwd = inputPassword.getText().toString().trim();
+                login(email, pwd);
+            }
+        });
+    }
+
+    private void login(String email,String pwd) {
+        if (email == null || email.length() <= 0){
+            Toast.makeText(this, "请输入邮箱", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if (pwd == null || pwd.length() <= 0) {
+            Toast.makeText(this,"请输入密码", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
     }
 
     private void setListeners(){
